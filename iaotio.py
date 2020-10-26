af42d00e46a57c03476c9e3ab98b67af658309a0
Matt Lam <59208036+rtkfan@users.noreply.github.com>
2020-10-25 18:07:44 -0700

    linting

--- iaotio.py
+++ iaotio.py
@@ -1,4 +1,4 @@
-import requests
+# import requests
 from bs4 import BeautifulSoup
 import locale
 import random
@@ -12,12 +12,13 @@ logging.basicConfig(format='%(asctime)s %(message)s',
 
 UNCOUNTED_BALLOTS = 600000  # absentee/mail-in ballots, est. from Elections BC
 NUM_TRIALS = 1000  # number of trials per electoral district
-BIAS = [-0.1,-0.09,-0.08,-0.07,-0.06,-0.05,-0.04,-0.03,-0.02,-0.01,
-        0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1]
+BIAS = [-0.1, -0.09, -0.08, -0.07, -0.06, -0.05, -0.04, -0.03, -0.02, -0.01,
+        0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
 # bias (+ is toward BC Libs, - is toward BC NDP)
 
-def assign_votes(party_list, split_electionday, num_votes, party_bias = 0):
-    outlist = [0,0,0,0,0]
+
+def assign_votes(party_list, split_electionday, num_votes, party_bias=0):
+    outlist = [0]*len(party_list)
     split_adjusted = copy.deepcopy(split_electionday)
     bias_votes = int(sum(split_electionday)*party_bias)
     split_adjusted[0] += bias_votes
@@ -28,7 +29,7 @@ def assign_votes(party_list, split_electionday, num_votes, party_bias = 0):
         split_absentee = [len(list(filter(lambda x: x == p, votelist)))
                           for p in party_list]
         split_total = [sum(x) for x in zip(split_adjusted, split_absentee)]
-        winner = max(range(len(split_total)),key = split_total.__getitem__)
+        winner = max(range(len(split_total)), key=split_total.__getitem__)
         outlist[winner] += 1
     return outlist
 
@@ -44,10 +45,10 @@ def assign_votes(party_list, split_electionday, num_votes, party_bias = 0):
 # -----------------------------------------------------------------------------
 
 # use cached results ----------------------------------------------------------
-with open('./datafile','r') as fread:
+with open('./datafile', 'r') as fread:
     pagetext = fread.read()
 
-soup = BeautifulSoup(pagetext,'html.parser')
+soup = BeautifulSoup(pagetext, 'html.parser')
 # -----------------------------------------------------------------------------
 
 headers = [i.get_text() for i in soup.find('thead').find_all('th')]
@@ -67,15 +68,16 @@ frac_ballots_electionday = [i/sum(vote_total_electionday)
 num_ballots_absentee = [int(UNCOUNTED_BALLOTS*i)
                         for i in frac_ballots_electionday]
 
-f = open('output.txt','w')
+f = open('output.txt', 'w')
 
 for ibias in BIAS:
     for i in range(len(districts)):
         logging.info(districts[i]+' @ bias='+str(ibias))
-        iwinner = max(range(len(vote_parties_electionday[i])),key = vote_parties_electionday[i].__getitem__)
+        iwinner = max(range(len(vote_parties_electionday[i])),
+                      key=vote_parties_electionday[i].__getitem__)
         out_list = assign_votes(parties, vote_parties_electionday[i],
-                                num_ballots_absentee[i],ibias)
-        new_winner = max(range(len(out_list)),key = out_list.__getitem__)
-        print(ibias, districts[i],parties[iwinner],parties[new_winner],
-              (NUM_TRIALS-out_list[iwinner])/NUM_TRIALS,sep='\t',
+                                num_ballots_absentee[i], ibias)
+        new_winner = max(range(len(out_list)), key=out_list.__getitem__)
+        print(ibias, districts[i], parties[iwinner], parties[new_winner],
+              (NUM_TRIALS-out_list[iwinner])/NUM_TRIALS, sep='\t',
               file=f)
