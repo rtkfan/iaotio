c75526b5ed419102141d0bce1ac99224032a6d68
Matt Lam <59208036+rtkfan@users.noreply.github.com>
2020-10-26 21:04:03 -0700

    run with 2000 trials (incl. output)

--- iaotio.py
+++ iaotio.py
@@ -11,7 +11,7 @@ logging.basicConfig(format='%(asctime)s %(message)s',
                     level=logging.INFO)
 
 UNCOUNTED_BALLOTS = 600000  # absentee/mail-in ballots, est. from Elections BC
-NUM_TRIALS = 1000  # number of trials per electoral district
+NUM_TRIALS = 2000  # number of trials per electoral district
 BIAS = [-0.1, -0.09, -0.08, -0.07, -0.06, -0.05, -0.04, -0.03, -0.02, -0.01,
         0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
 # bias (+ is toward BC Libs, - is toward BC NDP)
